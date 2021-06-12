ffe0ef3f3998af308ca8b261f7af0731df654b3a
knecht <ldj910505@naver.com>
2021-06-12 23:44:35 +0900

    <code>(fix) 사소한 부분 수정
    * 사소한 부분의 수정이 있었습니다.

--- src/webapp/src/main.js
+++ src/webapp/src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// router 활용
 import router from './router'
 // axios 활용
 import axios from 'axios'
