60fd992f5d182709595942ace93608d3d5a7f77b
knecht <ldj910505@naver.com>
2021-06-16 23:54:50 +0900

    <code>해커톤 페이지 일부 설명을 정리했습니다.

--- src/webapp/src/components/CoinManage.vue
+++ src/webapp/src/components/CoinManage.vue
@@ -64,7 +64,7 @@
           })
           .finally(() => {
             // alert("finally is called");
-          });
+					});
       },
       searchCoinByCoinName: function() {
         if (this.params.coinName == undefined || this.params.coinName == "") {
